811a289c113a376fd091c6c82c5ba717d8e4ba75
Jayanth <jayanthb3@quinnox.com>
2025-02-20 13:57:07 +0530

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+
+# Update the apt package list and install necessary dependencies for OpenCV and video processing
 apt-get update
-apt-get update && apt-get install -y libgl1
+
+# Install general OpenCV dependencies
+apt-get install -y \
+  libsm6 \
+  libxext6 \
+  libxrender-dev \
+  libglib2.0-0 \
+  libgthread-2.0-0 \
+  libfontconfig1 \
+  libx11-dev \
+  libjpeg-dev \
+  libpng-dev \
+  libtiff-dev \
+  libavcodec-dev \
+  libavformat-dev \
+  libswscale-dev \
+  libdcmtk14-dev \
+  libgstreamer1.0-0 \
+  libgstreamer-plugins-base1.0-0 \
+  libv4l-dev \
+  ffmpeg \
+  libeigen3-dev \
+  libgtk2.0-dev
+
+# Install Python dependencies (OpenCV and others)
+pip install --no-cache-dir opencv-python-headless
+
+# Start the application with Gunicorn and Uvicorn worker
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker app:app
